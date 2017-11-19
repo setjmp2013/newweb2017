7333835f450b62733b975f150d404bb6d6f0253a
Eric Allen Griff <setjmp@hotmail.com>
2017-11-19 17:14:56 -0500

    Added CSS Minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 // Gulp and related
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
+var cleanCSS    = require('gulp-clean-css');
+var rename      = require('gulp-rename');
 
 // Stylesheet Generation
 gulp.task('sass', function() {
     return gulp.src('src/scss/*.scss')
         .pipe(sass())
+        .pipe(gulp.dest('src/css'))
+        .pipe(cleanCSS())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('src/css'));
 });
